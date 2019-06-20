63648d66159c8bbc307c11d5b54e5423e70abc52
Angela Dyrda <adyrda2@gmail.com>
2019-06-20 11:14:31 -0500

    Add data to bar graph

--- SwiftChartsTutorial/BarChartViewController.swift
+++ SwiftChartsTutorial/BarChartViewController.swift
@@ -4,4 +4,24 @@ import Charts
 class BarChartViewController: UIViewController {
   @IBOutlet weak var barChartView: BarChartView!
 
+  var months: [String]!
+  var dataEntries: [BarChartDataEntry] = []
+
+  override func viewDidLoad() {
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+
+    customizeChart(dataPoints: months, values: unitsSold.map { Double($0) })
+  }
+
+  func customizeChart(dataPoints: [String], values: [Double]) {
+    for i in 0..<dataPoints.count {
+      let dataEntry = BarChartDataEntry(x: Double(i), y: Double(values[i]))
+      dataEntries.append(dataEntry)
+    }
+
+    let chartDataSet = BarChartDataSet(values: dataEntries, label: "Bar Chart View")
+    let chartData = BarChartData(dataSet: chartDataSet)
+    barChartView.data = chartData
+  }
 }
